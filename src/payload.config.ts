a73c975845058bdd96a40ba65e892553efdff775
quantum-grit <91589884+quantum-grit@users.noreply.github.com>
2026-05-11 15:44:05 +0300

    added config for self-signed cert of smtp server

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -64,6 +64,9 @@ const email = nodemailerAdapter(
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
           },
+          tls: {
+            rejectUnauthorized: process.env.SMTP_FORBID_SELF_SIGNED,
+          },
         },
       }
     : {
